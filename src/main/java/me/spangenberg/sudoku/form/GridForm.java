5112bdd818b593d7b890bad8f69d0d87a29c838b
JOHN SPANGENBERG <ja.spangenberg@gmail.com>
2017-06-11 14:07:02 +0200

    Removed unused property

--- src/main/java/me/spangenberg/sudoku/form/GridForm.java
+++ src/main/java/me/spangenberg/sudoku/form/GridForm.java
@@ -1,102 +1,183 @@
 package me.spangenberg.sudoku.form;
 
+import javax.validation.constraints.Digits;
+
 /**
  * @author John Spangenberg
  */
 public class GridForm {
 
+    @Digits(integer = 1, fraction = 0)
     private int a1;
+    @Digits(integer = 1, fraction = 0)
     private int a2;
+    @Digits(integer = 1, fraction = 0)
     private int a3;
+    @Digits(integer = 1, fraction = 0)
     private int a4;
+    @Digits(integer = 1, fraction = 0)
     private int a5;
+    @Digits(integer = 1, fraction = 0)
     private int a6;
+    @Digits(integer = 1, fraction = 0)
     private int a7;
+    @Digits(integer = 1, fraction = 0)
     private int a8;
+    @Digits(integer = 1, fraction = 0)
     private int a9;
 
+    @Digits(integer = 1, fraction = 0)
     private int b1;
+    @Digits(integer = 1, fraction = 0)
     private int b2;
+    @Digits(integer = 1, fraction = 0)
     private int b3;
+    @Digits(integer = 1, fraction = 0)
     private int b4;
+    @Digits(integer = 1, fraction = 0)
     private int b5;
+    @Digits(integer = 1, fraction = 0)
     private int b6;
+    @Digits(integer = 1, fraction = 0)
     private int b7;
+    @Digits(integer = 1, fraction = 0)
     private int b8;
+    @Digits(integer = 1, fraction = 0)
     private int b9;
 
+    @Digits(integer = 1, fraction = 0)
     private int c1;
+    @Digits(integer = 1, fraction = 0)
     private int c2;
+    @Digits(integer = 1, fraction = 0)
     private int c3;
+    @Digits(integer = 1, fraction = 0)
     private int c4;
+    @Digits(integer = 1, fraction = 0)
     private int c5;
+    @Digits(integer = 1, fraction = 0)
     private int c6;
+    @Digits(integer = 1, fraction = 0)
     private int c7;
+    @Digits(integer = 1, fraction = 0)
     private int c8;
+    @Digits(integer = 1, fraction = 0)
     private int c9;
 
-
+    @Digits(integer = 1, fraction = 0)
     private int d1;
+    @Digits(integer = 1, fraction = 0)
     private int d2;
+    @Digits(integer = 1, fraction = 0)
     private int d3;
+    @Digits(integer = 1, fraction = 0)
     private int d4;
+    @Digits(integer = 1, fraction = 0)
     private int d5;
+    @Digits(integer = 1, fraction = 0)
     private int d6;
+    @Digits(integer = 1, fraction = 0)
     private int d7;
+    @Digits(integer = 1, fraction = 0)
     private int d8;
+    @Digits(integer = 1, fraction = 0)
     private int d9;
 
+    @Digits(integer = 1, fraction = 0)
     private int e1;
+    @Digits(integer = 1, fraction = 0)
     private int e2;
+    @Digits(integer = 1, fraction = 0)
     private int e3;
+    @Digits(integer = 1, fraction = 0)
     private int e4;
+    @Digits(integer = 1, fraction = 0)
     private int e5;
+    @Digits(integer = 1, fraction = 0)
     private int e6;
+    @Digits(integer = 1, fraction = 0)
     private int e7;
+    @Digits(integer = 1, fraction = 0)
     private int e8;
+    @Digits(integer = 1, fraction = 0)
     private int e9;
 
+    @Digits(integer = 1, fraction = 0)
     private int f1;
+    @Digits(integer = 1, fraction = 0)
     private int f2;
+    @Digits(integer = 1, fraction = 0)
     private int f3;
+    @Digits(integer = 1, fraction = 0)
     private int f4;
+    @Digits(integer = 1, fraction = 0)
     private int f5;
+    @Digits(integer = 1, fraction = 0)
     private int f6;
+    @Digits(integer = 1, fraction = 0)
     private int f7;
+    @Digits(integer = 1, fraction = 0)
     private int f8;
+    @Digits(integer = 1, fraction = 0)
     private int f9;
 
+    @Digits(integer = 1, fraction = 0)
     private int g1;
+    @Digits(integer = 1, fraction = 0)
     private int g2;
+    @Digits(integer = 1, fraction = 0)
     private int g3;
+    @Digits(integer = 1, fraction = 0)
     private int g4;
+    @Digits(integer = 1, fraction = 0)
     private int g5;
+    @Digits(integer = 1, fraction = 0)
     private int g6;
+    @Digits(integer = 1, fraction = 0)
     private int g7;
+    @Digits(integer = 1, fraction = 0)
     private int g8;
+    @Digits(integer = 1, fraction = 0)
     private int g9;
 
+    @Digits(integer = 1, fraction = 0)
     private int h1;
+    @Digits(integer = 1, fraction = 0)
     private int h2;
+    @Digits(integer = 1, fraction = 0)
     private int h3;
+    @Digits(integer = 1, fraction = 0)
     private int h4;
+    @Digits(integer = 1, fraction = 0)
     private int h5;
+    @Digits(integer = 1, fraction = 0)
     private int h6;
+    @Digits(integer = 1, fraction = 0)
     private int h7;
+    @Digits(integer = 1, fraction = 0)
     private int h8;
+    @Digits(integer = 1, fraction = 0)
     private int h9;
 
+    @Digits(integer = 1, fraction = 0)
     private int i1;
+    @Digits(integer = 1, fraction = 0)
     private int i2;
+    @Digits(integer = 1, fraction = 0)
     private int i3;
+    @Digits(integer = 1, fraction = 0)
     private int i4;
+    @Digits(integer = 1, fraction = 0)
     private int i5;
+    @Digits(integer = 1, fraction = 0)
     private int i6;
+    @Digits(integer = 1, fraction = 0)
     private int i7;
+    @Digits(integer = 1, fraction = 0)
     private int i8;
+    @Digits(integer = 1, fraction = 0)
     private int i9;
 
-    private int numberOfEmptyCells;
     private String checkResult;
 
 
@@ -746,14 +827,6 @@ public class GridForm {
 
     public void setI9(int i9) {
         this.i9 = i9;
-    }
-
-    public int getNumberOfEmptyCells() {
-        return numberOfEmptyCells;
-    }
-
-    public void setNumberOfEmptyCells(int numberOfEmptyCells) {
-        this.numberOfEmptyCells = numberOfEmptyCells;
     }
 
     public String getCheckResult() {
